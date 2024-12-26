76eba64aa4751b5ad179b000e2b697b1f8ccd566
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-12-26 11:57:49 +0530

    Initial Commit- completed onboarding screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:eventify/views/onboarding_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,13 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Eventify",
+      title: 'EventiFy  ',
       theme: ThemeData(
-        textTheme: GoogleFonts.latoTextTheme(
-          Theme.of(context).textTheme,
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: OnbaordingScreen(),
+      home: OnboardingScreen(),
     );
   }
 }
